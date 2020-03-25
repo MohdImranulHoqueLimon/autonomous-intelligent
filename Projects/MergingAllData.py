7f8008330b86574c544702000c0c0ca46d950479
sehrishf <sehrish.fahim91@gmail.com>
2020-03-25 11:02:40 +0100

    ML work

--- Projects/MergingAllData.py
+++ Projects/MergingAllData.py
@@ -48,6 +48,69 @@ def str_column_to_int(dataset, column):
         row[column] = lookup[row[column]]
     return lookup
 
+# Split dataset by class then calculate statistics for each row
+def summarize_by_class(dataset):
+    separated = separate_by_class(dataset)
+    summaries = dict()
+    for class_value, rows in separated.items():
+        summaries[class_value] = summarize_dataset(rows)
+    return summaries
+
+# Calculate the mean, stdev and count for each column in a dataset
+def summarize_dataset(dataset):
+    summaries = [(mean(column), stdev(column), len(column)) for column in zip(*dataset)]
+    del (summaries[-1])
+    return summaries
+
+# Calculate the mean of a list of numbers
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
+
+
+# Calculate the standard deviation of a list of numbers
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([(x - avg) ** 2 for x in numbers]) / float(len(numbers) - 1)
+    return sqrt(variance)
+
+# Split the dataset by class values, returns a dictionary
+def separate_by_class(dataset):
+    separated = dict()
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        class_value = vector[-1]
+        if (class_value not in separated):
+            separated[class_value] = list()
+        separated[class_value].append(vector)
+    return separated
+
+# Calculate the probabilities of predicting each class for a given row
+def calculate_class_probabilities(summaries, row):
+    total_rows = sum([summaries[label][0][2] for label in summaries])
+    probabilities = dict()
+    for class_value, class_summaries in summaries.items():
+        probabilities[class_value] = summaries[class_value][0][2] / float(total_rows)
+        for i in range(len(class_summaries)):
+            mean, stdev, _ = class_summaries[i]
+            probabilities[class_value] *= calculate_probability(row[i], mean, stdev)
+    return probabilities
+
+# Calculate the Gaussian probability distribution function for x
+def calculate_probability(x, mean, stdev):
+    exponent = exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
+    return (1 / (sqrt(2 * pi) * stdev)) * exponent
+
+
+# Predict the class for a given row
+def predict(summaries, row):
+    probabilities = calculate_class_probabilities(summaries, row)
+    best_label, best_prob = None, -1
+    for class_value, probability in probabilities.items():
+        if best_label is None or probability > best_prob:
+            best_prob = probability
+            best_label = class_value
+    return best_label
+
 # Convert string column to float
 def str_column_to_float(dataset, column):
     for row in dataset:
@@ -57,14 +120,17 @@ wallFilename = 'ml/wallpeak.csv'
 humanFilename = 'ml/humanpeak.csv'
 carFilename = 'ml/carpeak.csv'
 
-allData = list()
+trainignData = list()
+test= list()
 
-allData = load_csv(wallFilename, allData)
-allData = load_csv(humanFilename, allData)
-allData = load_csv(carFilename, allData)
+trainignData = load_csv(wallFilename, trainignData)
+trainignData = load_csv(humanFilename, trainignData)
+trainignData = load_csv(carFilename, trainignData)
 
-
-
-
-
-
+test=trainignData.pop(100)
+test=test[0:50]
+# fit model
+model = summarize_by_class(trainignData)
+# predict the label
+label = predict(model, test)
+print('Data=%s, Predicted: %s' % (test, label))
